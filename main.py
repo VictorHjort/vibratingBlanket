f8a82b05ee343fcd78708945745d42fbfe087341
Victor Hjort <vhjort20@student.aau.dk>
2023-11-16 11:28:56 +0100

    updated main with defined functions

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ import time
 ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
 portsList = []
+
 #Vibration variables
+spots = ['LEFT','RIGHT','BACK','FRONT']
 intnesity = ['OFF','LOW','MEDIUM','HIGH']
+vibrationDictionary = {
+ 20 : (spots[0],intnesity[3]),
+ 45 : (spots[1],intnesity[2])
+}
+
+
 
 
 def choosePort():
@@ -23,54 +31,48 @@ def choosePort():
    portVar = "COM" + str(val)
  return portVar
 
-def playVideo(fileName):
-    # Create a VideoCapture object and read from input file 
-	cap = cv2.VideoCapture(fileName) 
-	frameNum = 0;
 
-	# Check if camera opened successfully 
-	if (cap.isOpened()== False): 
-		print("Error opening video file") 
-
-	#Start time measure
-	startTime = time.time()
-	# Read until video is completed 
-	while(cap.isOpened()): 
-		
-	# Capture frame-by-frame 
-		ret, frame = cap.read() 
-		if ret == True: 
-		# Display the resulting frame 
-			cv2.imshow('Frame', frame) 
-			print(frameNum)
-			frameNum += 1
-			nowTime = time.time()
-			if nowTime - startTime > 1:
-				print("1 second")
-				startTime = time.time()
-				frameNum = 0
-		# Press Q on keyboard to exit 
-			if cv2.waitKey(25) & 0xFF == ord('q'): 
-				break
-
-	# Break the loop 
-		else: 
-			break
-
-	# When everything done, release 
-	# the video capture object 
-	cap.release() 
-
-	# Closes all the frames 
-	cv2.destroyAllWindows()
-
-def sendCommand(vibrationIntensity):
+def sendCommand(cmd):
+ serialInst.write(cmd.encode('utf-8'))
  
-    	
 
-serialInst.baudrate = 9600
-serialInst.port = choosePort()
-serialInst.open()
+def playVideo(fileName, vibrationDict):
+ # Create a VideoCapture object and read from input file 
+ cap = cv2.VideoCapture(fileName) 
+ frameNum = 0;
 
+ # Check if camera opened successfully 
+ if (cap.isOpened()== False): 
+  print("Error opening video file") 
+ # Read until video is completed 
+ while(cap.isOpened()):
+  # Capture frame-by-frame 
+  ret, frame = cap.read() 
+  if ret == True: 
+   # Display the resulting frame 
+   cv2.imshow('Frame', frame) 
+   print(frameNum)
+   if frameNum in vibrationDict:
+    for frameNun, value in vibrationDict:
+     
+   frameNum += 1
+   # Press Q on keyboard to exit 
+   if cv2.waitKey(25) & 0xFF == ord('q'): 
+    break
 
-playVideo("world.mp4")
+  # Break the loop 
+  else: 
+   break
+
+ # When everything done, release 
+ # the video capture object 
+ cap.release() 
+
+ # Closes all the frames 
+ cv2.destroyAllWindows()
+
+def run():
+ serialInst.baudrate = 9600
+ serialInst.port = choosePort()
+ serialInst.open()
+ playVideo("world.mp4")
